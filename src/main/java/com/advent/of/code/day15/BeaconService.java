0b15927667da4ca7983e8fa1670fb48a4467a7d3
mantkowicz <kontakt@antkowicz.pl>
2022-12-18 10:34:36 +0100

    Day 15 refactoring

--- src/main/java/com/advent/of/code/day15/BeaconService.java
+++ src/main/java/com/advent/of/code/day15/BeaconService.java
@@ -5,7 +5,10 @@ import com.advent.of.code.Pair;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.abs;
 
@@ -13,6 +16,11 @@ public class BeaconService {
 
     public long countReservedAt(List<Pair<IntPair, IntPair>> input, int line) {
         final List<Pair<Long, Long>> ranges = new ArrayList<>();
+        long beaconsInRow = new HashSet<>(input.stream().map(Pair::getB).collect(Collectors.toList()))
+                .stream()
+                .filter(b -> b.getB().equals(line))
+                .collect(Collectors.toSet())
+                .size();
 
         for (Pair<IntPair, IntPair> pair : input) {
             final IntPair sensor = pair.getA();
@@ -21,7 +29,7 @@ public class BeaconService {
 
             if (abs(sensor.getB() - line) < distance) {
                 long rest = distance - abs(sensor.getB() - line);
-                ranges.add(new Pair<>(sensor.getB() - rest, sensor.getB() + rest));
+                ranges.add(new Pair<>(sensor.getA() - rest, sensor.getA() + rest));
             }
         }
 
@@ -30,14 +38,19 @@ public class BeaconService {
         long result = 0;
 
         for (int i = 0; i < ranges.size(); i++) {
-            result += ranges.get(i).getB() - ranges.get(i).getA() + 1;
-            for(int j = i + 1; j < ranges.size(); j++) {
-                if(i != j) {
-                    result -= Math.max(0, Math.min(ranges.get(i).getB(), ranges.get(j).getB()) - Math.max(ranges.get(i).getA(), ranges.get(j).getA()) + 1);
-                }
+            Set<Pair<Long, Long>> exclusive = new HashSet<>();
+            exclusive.add(ranges.get(i));
+
+            for (int j = i + 1; j < ranges.size(); j++) {
+                final Pair<Long, Long> current = ranges.get(i);
+                final Pair<Long, Long> next = ranges.get(j);
+                result -= Math.max(0, Math.min(current.getB(), next.getB()) - Math.max(current.getA(), next.getA()) + 1);
             }
+
+            result += exclusive.stream().mapToLong(r -> r.getB() - r.getA() + 1).sum();
         }
 
-        return result;
+        return result - beaconsInRow;
     }
 }
+//4200403 too low
